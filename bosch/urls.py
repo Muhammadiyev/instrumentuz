4cf7580a14889fb55303b56a1110e0724a1afdcb
Muhammadiyev <55634820+Muhammadiyev@users.noreply.github.com>
2019-09-25 16:16:15 +0500

    Update urls.py

--- bosch/urls.py
+++ bosch/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from upload.views import image_upload
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,7 +9,6 @@ urlpatterns = [
     path('api/v1/', include('instruments.urls')),
     path('api/v1/', include('dictionary.urls')),
     path('api/v1/news/', include('comments.urls')),
-    path('', image_upload, name='upload'),
 ]
 
 if bool(settings.DEBUG):
